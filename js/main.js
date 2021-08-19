939549a2a00fc7a48441bffe9d6e7c1d31e7a71f
ngimdock <ngimdockzemfack@gmail.com>
2021-08-19 21:32:24 +0100

    Terminaison du projet

--- js/main.js
+++ js/main.js
@@ -1,3 +1,23 @@
 
+const tabsItems = document.querySelectorAll(".tab-item");
+const tabsContentItems = document.querySelectorAll(".tab-content-item");
 
-console.log("dans le main");
+// select tab content item
+function handleSelectItem(event){
+    removeBorder();
+    removeShow();
+    this.classList.add("tab-border")
+    const tabContentItem = document.querySelector(`#${this.id}-content`);
+    tabContentItem.classList.add("show");
+};
+
+function removeBorder() {
+    tabsItems.forEach(item => item.classList.remove("tab-border"));
+}
+
+function removeShow() {
+    tabsContentItems.forEach(item => item.classList.remove("show"));
+}
+
+
+tabsItems.forEach(item => item.addEventListener("click", handleSelectItem))
